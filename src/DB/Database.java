0f5213d892b79fce1d441b1992f826ec1a9095bb
darkllen <yankiniigor@gmail.com>
2019-01-17 12:54:03 +0200

    1

--- src/DB/Database.java
+++ src/DB/Database.java
@@ -1,7 +1,12 @@
 package DB;
 
+import Model.Cafedra;
+import Model.Faculty;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Database {
     private static Statement statement;
@@ -18,6 +23,39 @@ public class Database {
             e.printStackTrace();
         }
     }
+
+    public ArrayList<Faculty> getFaculties(){
+        ArrayList<Faculty> arrayList = new ArrayList();
+        String query = "Select * from `Faculty`";
+        try {
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()){
+                Faculty faculty = new Faculty(rs.getInt("id"),rs.getString("name"));
+                arrayList.add(faculty);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return arrayList;
+    }
+
+    public ArrayList<Cafedra> getCafedrasByFacultyId(int facultyId){
+        ArrayList<Cafedra> arrayList = new ArrayList();
+        String query = "Select * from `Cafedra` where id_faculty =" +facultyId;
+        try {
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()){
+                Cafedra cafedra = new Cafedra(rs.getInt("id"),rs.getString("name"),facultyId);
+                arrayList.add(cafedra);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return arrayList;
+    }
+
+    
+
     public void insertNewCafedra(int idFaculty, String cafedraName){
         String query =  "INSERT INTO `Cafedra` (`id_faculty`, `name`) VALUES (\""+idFaculty+"\",\""+cafedraName+"\")";
         try {
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import DB.Connect;
+import DB.Database;
 
 public class Main {
     public static void main(String[] args) {
-
+        Connect connect = new Connect();
+        System.out.println("s");
+        Database database = new Database(connect.connectToDB());
+        System.out.println("s");
+        System.out.println(database.getFaculties().get(0).getName());
 }
 
 }
